50d36aa9d2f7538f785a3ca61ee008e333bf8135
mzrtamp <mozartrafi@stegripe.org>
2025-08-31 15:43:09 +0700

    feat: display manager

---
+++ include/display_manager.h
@@ -0,0 +1,38 @@
+#ifndef DISPLAY_MANAGER_H
+#define DISPLAY_MANAGER_H
+
+#include <Arduino.h>
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ILI9341.h>
+#include "display_texts.h"
+
+// Pin definitions untuk LCD TFT
+#define TFT_DC 4
+#define TFT_CS 5
+#define TFT_MOSI 23
+#define TFT_SCK  18
+#define TFT_RST  2
+#define TFT_MISO 19
+
+// Colors
+#define COLOR_BACKGROUND ILI9341_BLACK
+#define COLOR_TEXT ILI9341_CYAN
+#define COLOR_CLEAR ILI9341_BLACK
+
+// Global LCD object
+extern Adafruit_ILI9341 tft;
+
+// Fungsi inisialisasi
+void initDisplay();
+
+// Fungsi untuk clear screen dengan cara overwrite text hitam
+void clearScreen();
+
+// Fungsi untuk menampilkan menu utama
+void tampilkanMenuUtama();
+
+// Fungsi helper untuk print text dengan posisi dan size
+void printText(int x, int y, int size, const char* text, uint16_t color = COLOR_TEXT);
+
+#endif
---
+++ src/display_manager.cpp
@@ -0,0 +1,60 @@
+#include "display_manager.h"
+
+// Inisialisasi object LCD TFT
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCK, TFT_RST, TFT_MISO);
+
+void initDisplay() {
+    Serial.println("Inisialisasi LCD TFT...");
+    tft.begin();
+    tft.setRotation(1);  // Landscape mode
+    tft.fillScreen(COLOR_BACKGROUND);
+    Serial.println("LCD TFT berhasil diinisialisasi");
+}
+
+void printText(int x, int y, int size, const char* text, uint16_t color) {
+    tft.setCursor(x, y);
+    tft.setTextSize(size);
+    tft.setTextColor(color);
+    tft.print(text);
+}
+
+void clearScreen() {
+    // Clear dengan cara overwrite text dengan warna hitam
+    // Header top
+    printText(LAYOUT_HEADER_TOP.x, LAYOUT_HEADER_TOP.y, LAYOUT_HEADER_TOP.size, MENU_HEADER_LINE, COLOR_CLEAR);
+    
+    // Title
+    printText(LAYOUT_TITLE_1.x, LAYOUT_TITLE_1.y, LAYOUT_TITLE_1.size, MENU_TITLE_LINE1, COLOR_CLEAR);
+    printText(LAYOUT_TITLE_2.x, LAYOUT_TITLE_2.y, LAYOUT_TITLE_2.size, MENU_TITLE_LINE2, COLOR_CLEAR);
+    
+    // Header bottom  
+    printText(LAYOUT_HEADER_BOTTOM.x, LAYOUT_HEADER_BOTTOM.y, LAYOUT_HEADER_BOTTOM.size, MENU_HEADER_LINE, COLOR_CLEAR);
+    
+    // Menu options
+    printText(LAYOUT_OPTION_1.x, LAYOUT_OPTION_1.y, LAYOUT_OPTION_1.size, MENU_OPTION_1, COLOR_CLEAR);
+    printText(LAYOUT_OPTION_2.x, LAYOUT_OPTION_2.y, LAYOUT_OPTION_2.size, MENU_OPTION_2, COLOR_CLEAR);
+    printText(LAYOUT_OPTION_3.x, LAYOUT_OPTION_3.y, LAYOUT_OPTION_3.size, MENU_OPTION_3, COLOR_CLEAR);
+    printText(LAYOUT_OPTION_3_SUB.x, LAYOUT_OPTION_3_SUB.y, LAYOUT_OPTION_3_SUB.size, MENU_OPTION_3_SUB, COLOR_CLEAR);
+    printText(LAYOUT_OPTION_4.x, LAYOUT_OPTION_4.y, LAYOUT_OPTION_4.size, MENU_OPTION_4, COLOR_CLEAR);
+    printText(LAYOUT_OPTION_5.x, LAYOUT_OPTION_5.y, LAYOUT_OPTION_5.size, MENU_OPTION_5, COLOR_CLEAR);
+}
+
+void tampilkanMenuUtama() {
+    // Header top
+    printText(LAYOUT_HEADER_TOP.x, LAYOUT_HEADER_TOP.y, LAYOUT_HEADER_TOP.size, MENU_HEADER_LINE);
+    
+    // Title
+    printText(LAYOUT_TITLE_1.x, LAYOUT_TITLE_1.y, LAYOUT_TITLE_1.size, MENU_TITLE_LINE1);
+    printText(LAYOUT_TITLE_2.x, LAYOUT_TITLE_2.y, LAYOUT_TITLE_2.size, MENU_TITLE_LINE2);
+    
+    // Header bottom
+    printText(LAYOUT_HEADER_BOTTOM.x, LAYOUT_HEADER_BOTTOM.y, LAYOUT_HEADER_BOTTOM.size, MENU_HEADER_LINE);
+    
+    // Menu options
+    printText(LAYOUT_OPTION_1.x, LAYOUT_OPTION_1.y, LAYOUT_OPTION_1.size, MENU_OPTION_1);
+    printText(LAYOUT_OPTION_2.x, LAYOUT_OPTION_2.y, LAYOUT_OPTION_2.size, MENU_OPTION_2);
+    printText(LAYOUT_OPTION_3.x, LAYOUT_OPTION_3.y, LAYOUT_OPTION_3.size, MENU_OPTION_3);
+    printText(LAYOUT_OPTION_3_SUB.x, LAYOUT_OPTION_3_SUB.y, LAYOUT_OPTION_3_SUB.size, MENU_OPTION_3_SUB);
+    printText(LAYOUT_OPTION_4.x, LAYOUT_OPTION_4.y, LAYOUT_OPTION_4.size, MENU_OPTION_4);
+    printText(LAYOUT_OPTION_5.x, LAYOUT_OPTION_5.y, LAYOUT_OPTION_5.size, MENU_OPTION_5);
+}
